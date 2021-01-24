5e80819b8340d7e3e10b3799155f19d31f8d801f
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2021-01-25 03:29:10 +0800

    Set dialog style to Undecorated

--- src/main/scala/com/melvic/catana/view/users/Register.scala
+++ src/main/scala/com/melvic/catana/view/users/Register.scala
@@ -7,11 +7,12 @@ import scalafx.Includes._
 import scalafx.scene.control.ButtonBar.ButtonData
 import scalafx.scene.control.{ButtonType, Dialog, TextField}
 import scalafx.scene.layout.VBox
-import scalafx.stage.Stage
+import scalafx.stage.{Stage, StageStyle}
 
 object Register {
   def buildDialog(implicit stage: Stage) = new Dialog[User] {
     initOwner(stage)
+    initStyle(StageStyle.Undecorated)
     title = "Registration Form"
     headerText = "Registration Form"
   }
@@ -29,8 +30,7 @@ object Register {
     val passwordField = defaultPasswordField
     val emailField = new TextField {
       promptText = "Email"
-      maxWidth = 375
-      prefWidth = 375
+      styleClass += "email-field"
     }
     val nameField = promptField("Name")
     val addressField = promptField("Address")
